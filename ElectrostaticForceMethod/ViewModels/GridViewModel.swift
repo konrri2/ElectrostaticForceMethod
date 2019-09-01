0e4ef03aa90073d3df7d5d4ae0453f62a3608a56
konrri <konrri@gmail.com>
2019-09-01 09:04:17 +0200

    power operator

--- ElectrostaticForceMethod/ViewModels/GridViewModel.swift
+++ ElectrostaticForceMethod/ViewModels/GridViewModel.swift
@@ -10,9 +10,16 @@ import Foundation
 import SpriteKit
 
 
+infix operator **
+func ** (_ base: Double, _ exp: Double) -> Double {
+    return pow(base, exp)
+}
+
+
 ///draw grid and cordinates
 class GridViewModel {
     var pricesText = [String]()
+    
     
     init() {
         for i in 0...12 {

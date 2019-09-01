b531e736128d32862bab49b138df94d6223ff5c3
konrri <konrri@gmail.com>
2019-09-01 15:38:48 +0200

    tests

---
+++ gitCopy.sh
@@ -0,0 +1,6 @@
+git pull
+rm -r /Users/konrri/Work/ElectrostaticForceMethod/Electro*
+cp -r Electro* /Users/konrri/Work/ElectrostaticForceMethod
+cd /Users/konrri/Work/ElectrostaticForceMethod
+git commit -m "$0"
+git push

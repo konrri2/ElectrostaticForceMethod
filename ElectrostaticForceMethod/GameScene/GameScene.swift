ead42d36419084929dd3d2c269fc75c5de24c541
Konrad Leszczyński <konrri@gmail.com>
2019-09-02 11:13:25 +0200

    camera picnch geasture

--- ElectrostaticForceMethod/GameScene/GameScene.swift
+++ ElectrostaticForceMethod/GameScene/GameScene.swift
@@ -10,21 +10,13 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    //
-    private var label : SKLabelNode?
+ 
     private var spinnyNode : SKShapeNode?
-    
+    var previousCameraScale = CGFloat()
     private var grid = GridViewModel()
+    let cameraNode = SKCameraNode()
     
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
         // Create shape node to use during mouse interaction
         let w = (self.size.width + self.size.height) * 0.05
         self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
@@ -43,6 +35,24 @@ class GameScene: SKScene {
             self.addChild(l)
         }
         
+        let pinchGesture = UIPinchGestureRecognizer()
+        pinchGesture.addTarget(self, action: #selector(pinchGestureAction(_:)))
+        view.addGestureRecognizer(pinchGesture)
+        
+        self.camera = cameraNode
+    }
+    
+    
+    
+    
+    @objc func pinchGestureAction(_ sender: UIPinchGestureRecognizer) {
+        guard let camera = self.camera else {
+            return
+        }
+        if sender.state == .began {
+            previousCameraScale = camera.xScale
+        }
+        camera.setScale(previousCameraScale * 1 / sender.scale)
     }
     
     
@@ -71,9 +81,6 @@ class GameScene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
         
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }

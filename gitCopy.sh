e82df64e5758b21d50d72a8d08600e2b1eaaeab2
konrri <konrri@gmail.com>
2019-10-15 14:49:51 +0200

    gitCopy - remove test

--- gitCopy.sh
+++
@@ -1,6 +0,0 @@
-git pull
-rm -r /Users/konrri/Work/ElectrostaticForceMethod_github/Electro*
-cp -r Electro* /Users/konrri/Work/ElectrostaticForceMethod_github
-cd /Users/konrri/Work/ElectrostaticForceMethod_github
-git commit -a -m "$1"
-git push

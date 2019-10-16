9756ad2c25fa71827aa5678e4092372e797ed3ef
konrri <konrri@gmail.com>
2019-10-16 13:23:09 +0200

    adding ranaming files

---
+++ ElectrostaticForceMethod/Common/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  ElectrostaticForceMethod
+//
+//  Created by Konrad Leszczyński on 16/10/2019.
+//  Copyright © 2019 konrri. All rights reserved.
+//
+
+import SpriteKit
+
+//GLOBAL VARIABLES
+
+
+///number of points to multiply on price axis
+let priceAxisScale = 200.0
---
+++ ElectrostaticForceMethod/ViewModels/FeedbackViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  FeedbackViewModel.swift
+//  ElectrostaticForceMethod
+//
+//  Created by Konrad Leszczyński on 16/10/2019.
+//  Copyright © 2019 konrri. All rights reserved.
+//
+
+import Foundation

386c6b37c8b87aeea04d9e0a78393af61b00a940
konrri <konrri@gmail.com>
2019-11-23 21:06:33 +0100

    distances wrong test

--- ElectrostaticForceMethodTests/EfmPhysicsTests.swift
+++ ElectrostaticForceMethodTests/EfmPhysicsTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import ElectrostaticForceMethod
 
 class EfmPhysicsTests: XCTestCase {
 
@@ -18,16 +19,17 @@ class EfmPhysicsTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    ///figure 2.a is in section 4.1 of the article
+    func testForFigure2a() {
+        let qt = Feedback(isPositive: false, type: .testCharge, price: 6.0)
+        let q1 = Feedback(isPositive: true, price: 2.0)
+        let q2 = Feedback(isPositive: true, price: 10.0)
+        
+        let dist1 = qt.distance(to: q1)
+        let dist2 = qt.distance(to: q2)
+        log("distance 1 = \(dist1);   dist2=\(dist1)")
+        XCTAssertEqual(dist1, dist2, "distances should be equal")
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 
 }

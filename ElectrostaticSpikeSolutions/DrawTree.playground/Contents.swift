82d22934cc088085c24c3d6527fe2294c2c2189b
konrri <konrri@gmail.com>
2019-11-30 15:49:41 +0100

    kurza stopa - nie kumam tej kartografii

--- ElectrostaticSpikeSolutions/DrawTree.playground/Contents.swift
+++ ElectrostaticSpikeSolutions/DrawTree.playground/Contents.swift
@@ -17,13 +17,27 @@ class GameScene: SKScene {
             return true
         }
     }
+    
+    func drawNode(on parent: SKNode) {
+        let label = SKLabelNode(text: "test")
+        label.name = "testLabel"
+        parent.addChild(label)
+        logCoordinates(label)
+        logCoordinates(parent)
+    }
+}
+
+func logCoordinates(_ node: SKNode) {
+    print("\(node.name) frame = \(node.frame) position=\(node.position)  ")
 }
 
 // Load the SKScene from 'GameScene.sks'
 let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 640, height: 480))
 if let scene = GameScene(fileNamed: "GameScene") {
     // Set the scale mode to scale to fit the window
-    scene.scaleMode = .aspectFill
+    //?? //scene.scaleMode = .aspectFill
+    scene.name = "scene node"
+    scene.drawNode(on: scene)
     
     // Present the scene
     sceneView.presentScene(scene)

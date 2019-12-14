1246fb5079e492df914923e7828a338f16ab0707
konrri <konrri@gmail.com>
2019-12-14 08:28:50 +0100

    categorie samples

--- ElectrostaticForceMethod/Models/CategoryTree.swift
+++ ElectrostaticForceMethod/Models/CategoryTree.swift
@@ -7,3 +7,15 @@
 //
 
 import Foundation
+
+
+var categoriesSamples = [
+  //           "Allegro   Sport i turystyka   Rowery i akcesoria   Rowery   Przełajowe, Gravel",
+            "Allegro   Elektronika   Telefony i Akcesoria   Smartfony i telefony komórkowe   Apple   iPhone XS",
+            "Allegro   Elektronika   Komputery   Laptopy   Apple",
+            "Allegro   Elektronika   Komputery   Laptopy   Dell",
+  //          "Allegro   Elektronika   Komputery   Laptopy   HP",
+ //           "Allegro   Elektronika   RTV i AGD   AGD wolnostojące   Pralko-suszarki",
+            "Allegro   Elektronika   Fotografia   Aparaty cyfrowe   Lustrzanki",
+            "Allegro   Dom i Ogród   Meble   Salon   Stoły" //copy to check if it will not repeat
+]

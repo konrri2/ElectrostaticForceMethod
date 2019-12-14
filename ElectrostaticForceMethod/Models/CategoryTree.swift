4c37b00d2d1b22b5115c4872e84d0f0519a3037e
konrri <konrri@gmail.com>
2019-12-14 08:26:27 +0100

    moving draw tree from playground to category

---
+++ ElectrostaticForceMethod/Models/CategoryTree.swift
@@ -0,0 +1,9 @@
+//
+//  CategoryTree.swift
+//  ElectrostaticForceMethod
+//
+//  Created by Konrad Leszczyński on 14/12/2019.
+//  Copyright © 2019 konrri. All rights reserved.
+//
+
+import Foundation

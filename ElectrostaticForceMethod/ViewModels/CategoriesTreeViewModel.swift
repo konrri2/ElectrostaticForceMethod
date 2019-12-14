49adf3059f513db613a39e01029dcc06f21d2037
konrri <konrri@gmail.com>
2019-12-14 08:37:38 +0100

    CategoriesTreeViewModel

--- ElectrostaticForceMethod/ViewModels/CategoriesTreeViewModel.swift
+++ ElectrostaticForceMethod/ViewModels/CategoriesTreeViewModel.swift
@@ -7,3 +7,83 @@
 //
 
 import Foundation
+import SpriteKit
+
+class CategoriesTreeViewModel {
+    
+    func drawBranch(node: TreeNode, frame: CGRect) {
+        let rect = frame
+        let button = makeButton(treeNode: node, rect: rect)
+        scene?.addChild(button)
+        if node.level >= 2 {
+            return  //don't draw to deap
+        }
+        else if node.children.count <= 0  {
+            return //leaf
+        }
+        else {
+            let childCount = CGFloat(node.children.count)
+            let segmentHeight = rect.height / childCount
+            let xOffset = rect.width / 4
+            var y = rect.origin.y
+            let x = rect.origin.x + xOffset
+            for ch in node.children {
+                let childRect = CGRect(x: x, y: y, width: rect.width - xOffset, height: segmentHeight)
+                drawBranch(node: ch, frame: childRect)
+                y += segmentHeight
+                
+                drawLine(fromX: rect.midX, fromY: rect.midY, toX: childRect.midX, toY: childRect.midY)
+            }
+        }
+    }
+    
+    private func drawLine(fromX: CGFloat, fromY: CGFloat, toX: CGFloat, toY: CGFloat) {
+        let yourline = SKShapeNode()
+        let pathToDraw = CGMutablePath()
+        pathToDraw.move(to: CGPoint(x: fromX, y: fromY))
+        pathToDraw.addLine(to: CGPoint(x: toX, y: toY))
+        yourline.path = pathToDraw
+        yourline.strokeColor = SKColor.red
+        addChild(yourline)
+    }
+    
+    func makeButton(treeNode: TreeNode, rect: CGRect) -> SKSpriteNode {
+        var label: SKLabelNode
+        
+        
+        let zPosition = CGFloat(treeNode.level*2)
+        
+        let button = SKSpriteNode(imageNamed: "TransparentHalfCircle")
+        button.position = CGPoint(x: rect.midX, y: rect.midY)
+        button.size = rect.size
+        button.name = treeNode.name
+        button.zPosition = zPosition
+        
+        label = SKLabelNode(fontNamed: "Arial")
+        label.fontSize = 12
+        label.name = "label:\(treeNode.name)"
+        
+        if treeNode.name.contains(" ") {// many words in name
+            let text = treeNode.name
+                    .replacingOccurrences(of: " i ", with: "\n")
+                    .replacingOccurrences(of: ", ", with: "\n")
+            label.text = text
+            label.numberOfLines = 2
+        }
+        else {
+            label.text = treeNode.name
+        }
+        label.zPosition = zPosition + 1
+        
+        //label.position = CGPoint(x: -(rect.width/3), y: 0)  //force move to the left
+
+        //label.zRotation = CGFloat.pi / 4
+        button.addChild(label)
+        
+        logCoordinates(label)
+        logCoordinates(button)
+        return button
+    }
+
+
+}

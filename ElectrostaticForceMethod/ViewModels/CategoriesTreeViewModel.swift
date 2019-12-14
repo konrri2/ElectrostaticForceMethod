6544803aa31ce09a8c4eb250ed207365d00a57f8
konrri <konrri@gmail.com>
2019-12-14 08:34:12 +0100

    CategoriesTreeViewModel

---
+++ ElectrostaticForceMethod/ViewModels/CategoriesTreeViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  CategoriesTreeViewModel.swift
+//  ElectrostaticForceMethod
+//
+//  Created by Konrad Leszczyński on 14/12/2019.
+//  Copyright © 2019 konrri. All rights reserved.
+//
+
+import Foundation
